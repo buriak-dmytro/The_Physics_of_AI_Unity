fdf85f606d443f3cbd978160f9d29eb9ed2875da
Dmytro Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2024-03-16 22:56:11 +0200

    First commit, fully done

---
+++ Assets/AccelerationAndTrajectoriesTutorial/Tanks/Scripts/Shell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+    private float speed = 0.0f;
+    private float ySpeed = 0.0f;
+    private float mass = 10.0f;
+    private float force = 2.0f;
+    private float drag = 1.0f;
+    private float gravity = -9.8f;
+    private float gAcc;
+    private float acceleration;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceleration = force / mass;
+        speed += acceleration * 1.0f;
+        gAcc = gravity / mass;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        speed *= (1 - Time.deltaTime * drag);
+        ySpeed += gAcc * Time.deltaTime;
+        this.transform.Translate(0, ySpeed, speed);
+    }
+}
---
+++ Assets/TimeTutorial/Walk/Script/SecondsUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private float timeStartOffset = 0;
+    private bool gotStartTime = false;
+
+    void Update()
+    {
+        if (!gotStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+
+        this.transform.position = new Vector3(this.transform.position.x,
+                                              this.transform.position.y,
+                                              (Time.realtimeSinceStartup - timeStartOffset) * speed);
+    }
+}
